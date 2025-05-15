d03b8b2bf60314be463c8bf6608f8aaa125c76d0
Andrey A. <aandrew773@gmail.com>
2025-05-16 00:02:57 +0500

    15th commit (added CORSMiddleware)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from app.routers.v1 import auth, category, permission, products, reviews
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+import os
+
 
 # Создаём основное приложение
 app = FastAPI(
@@ -14,21 +20,41 @@ app = FastAPI(
 # Создаём подприложение для версии v1
 app_v1 = FastAPI()
 
+# Монтируем статические файлы из директории 'static' по пути '/static'
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Настройка шаблонизатора Jinja2
+templates_dir = os.path.join(os.path.dirname(__file__), "templates")
+templates = Jinja2Templates(directory=templates_dir)
 
 # Определяем маршрут приветствия для основного приложения
-@app.get("/")
-async def welcome() -> dict:
-    return {"message": "Welcome to FastAPI Ecommerce App!"}
+@app.get("/", response_class=HTMLResponse)
+async def welcome(request: Request):
+    return templates.TemplateResponse("test_cors.html", {"request": request})
 
 # Добавляем маршруты к подприложению v1
-app_v1.include_router(category.router, prefix="/v1")
-app_v1.include_router(products.router, prefix="/v1")
-app_v1.include_router(auth.router, prefix="/v1")
-app_v1.include_router(permission.router, prefix="/v1")
-app_v1.include_router(reviews.router, prefix="/v1")
+app_v1.include_router(category.router)
+app_v1.include_router(products.router)
+app_v1.include_router(auth.router)
+app_v1.include_router(permission.router)
+app_v1.include_router(reviews.router)
 
 # Монтируем подприложения к основному приложению
 app.mount('/v1', app_v1)
+
+# Настройка CORS
+origins = [
+    "http://localhost:3000",
+    "https://example.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # Разрешённые источники
+    allow_credentials=True,         # Разрешить куки и авторизацию
+    allow_methods=["*"],            # Разрешённые методы (GET, POST, PUT, DELETE и т.д.)
+    allow_headers=["*"]             # Разрешённые заголовки
+)
 
 
 # Запуск приложения (если запускаем как скрипт)
