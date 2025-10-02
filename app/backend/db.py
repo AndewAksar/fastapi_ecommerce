5975fced2c3ecdf62731aef00ad43672f7a9c298
Andrew A. <aandrew773@gmail.com>
2025-10-02 20:43:03 +0500

    33th commit (Add load_dotenv to db.py)

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
